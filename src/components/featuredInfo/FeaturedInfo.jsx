bd6400d11137cd63575d178132f186b5f827557c
Fatimazahra2003 <fatimazahrawahia@gmail.com>
2022-02-09 19:46:38 +0100

    edited

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,7 +1,26 @@
 import "./featuredInfo.css";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function FeaturedInfo() {
+  const [income,setIncome]=useState([])
+  const [perc,setPerc]=useState(0)
+
+
+  useEffect(()=>{
+    const getIncome=async()=>{
+      try{
+        const res=await userRequest.get("orders/income")
+        setIncome(res.data)
+        setPerc((res.data[1].total*100)/res.data[0].total-100)
+      }catch{}
+    }
+    getIncome()
+  },[])
+
+  console.log(perc)
   return (
     <div className="featured">
       <div className="featuredItem">
