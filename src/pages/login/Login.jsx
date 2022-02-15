c4d9475a2dbe7c3f01a5dadba19f290e170729bd
Fatimazahra2003 <fatimazahrawahia@gmail.com>
2022-02-15 16:25:00 +0100

    addBG

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
-import {useState}  from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {login} from "../../redux/apiCalls";
+import { login } from "../../redux/apiCalls";
+import styled from "styled-components";
+
 //import styled from "styled-components";
 
 // const Button = styled.button`
@@ -16,30 +18,38 @@ import {login} from "../../redux/apiCalls";
 //         cursor:not-allowed;
 //     }
 // `;
-const Login =()=>{
+const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
 
-    const handleClick = (e)=>{
+    const handleClick = (e) => {
         e.preventDefault()
-        login(dispatch,{username,password});
+        login(dispatch, { username, password });
     };
-    return(
-        <div 
-        style={{
-            height: "100vh",
-            display:"flex",
-            flexDirection:"column",
-            alignItems: "center",
-            justifyContent: "center",
+  
+    return (
 
-            
-        
-        }}>
-            <input  style={{padding:10, marginBottom:20 }} type="text" placeholder="username" onChange={e=>setUsername(e.target.value)}/>
-            <input style={{ padding:10, marginBottom:20}} type="password" placeholder="password"  onChange={e=>setPassword(e.target.value)}/>
-            <button onClick={handleClick} style={{padding:10, width:100,}}> Login </button>
+        <div 
+            style={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundImage: `url("https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")`,
+                backgroundSize: "cover",
+                backgroundPosition: 'center',
+                width: '100vw',
+                height: '100vh',
+                
+
+
+
+            }}>
+            <input style={{ padding: 10, marginBottom: 20 }} type="text" placeholder="username" onChange={e => setUsername(e.target.value)} />
+            <input style={{ padding: 10, marginBottom: 20 }} type="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+            <button onClick={handleClick} style={{ padding: 10, width: 100,backgroundColor:"black",color:"white" }}> Login </button>
             {/* <Button onClick={handleClick} style={{padding:10, width:100,}}> Login</Button> */}
         </div>
     );
